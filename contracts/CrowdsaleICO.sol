a603029efccf929c88227e56561919da057b6f89
nileshpanhale1601 <nileshpanhale1601@gmail.com>
2021-05-10 17:43:44 +0530

    Whitelist investor error removed and ETH/USD price contract included

--- contracts/CrowdsaleICO.sol
+++ contracts/CrowdsaleICO.sol
@@ -1,12 +1,64 @@
 pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/smartcontractkit/chainlink/blob/master/evm-contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
-contract QuillHashToken is ERC20 {
+contract Whitelisted {
+    mapping(address => bool) public whitelist;
+
+    modifier isWhitelisted(address _investor) {
+        require(whitelist[_investor]);
+        _;
+    }
+
+    function add(address _investor) public {
+        whitelist[_investor] = true;
+    }
+
+    function addMultiple(address[] memory _investors) external {
+        for (uint256 i = 0; i < _investors.length; i++) {
+            whitelist[_investors[i]];
+        }
+    }
+
+    function remove(address _investor) external {
+        whitelist[_investor] = false;
+    }
+}
+
+contract USDconverter {
+    AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Network: Rinkeby
+     * Aggregator: ETH/USD
+     * Address: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+     */
+    constructor() public {
+        priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getThePrice() public view returns (int256) {
+        (
+            uint80 roundID,
+            int256 price,
+            uint256 startedAt,
+            uint256 timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+}
+
+contract QuillHashToken is ERC20, Whitelisted, USDconverter {
     string _name = "XToken";
     string _symbol = "XT";
     uint256 totalToken = 50000000000;
-    // uint tokenPrice = 0.001;
     address owner;
 
     uint256 public reserveWallet = 30;
@@ -34,5 +86,18 @@ contract QuillHashToken is ERC20 {
     {
         uint256 result = (_totalTotal * _share) / 100;
         return (result);
+    }
+
+    modifier isOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function addInvestor(address _investor) public isOwner {
+        add(_investor);
+    }
+
+    function getPrice() public {
+        int256 price = getThePrice();
     }
 }
